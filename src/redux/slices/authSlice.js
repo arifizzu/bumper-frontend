677ee9d720accf4a249614d40d02a93ad866e927
arifizzu <arif.izzu97@gmail.com>
2024-03-19 03:09:48 +0800

    use localStorage to store isAuthenticated instead of state

--- src/redux/slices/authSlice.js
+++ src/redux/slices/authSlice.js
@@ -2,7 +2,6 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   user: null,
-  isAuthenticated: false,
   loading: false,
   error: null,
 };
@@ -18,17 +17,15 @@ export const authSlice = createSlice({
     },
     loginSuccess: (state, action) => {
       state.loading = false;
-      state.isAuthenticated = true;
+      localStorage.setItem("isAuthenticated", "true");
       state.user = action.payload.user;
       console.log("action", action.payload);
-      // console.log("state.user", state.user);
       // console.log("loginSuccess");
     },
     loginFailure: (state, action) => {
       state.loading = false;
       state.error = action.payload;
       // console.log("loginFailure");
-      // console.log("state.error", state.error);
     },
     logoutStart: (state) => {
       state.loading = true;
@@ -37,16 +34,14 @@ export const authSlice = createSlice({
     },
     logoutSuccess: (state) => {
       state.loading = false;
-      state.isAuthenticated = false;
+      localStorage.removeItem("isAuthenticated");
       state.user = null;
-      // console.log("state.user", state.user);
       // console.log("logoutSuccess");
     },
     logoutFailure: (state, action) => {
       state.loading = false;
       state.error = action.payload;
       // console.log("loginFailure");
-      // console.log("state.error", state.error);
     },
   },
 });
