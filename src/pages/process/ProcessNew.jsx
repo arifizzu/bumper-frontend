ae1044a5e4989f2e7a00edd79c9c660d93443e48
arifizzu <arif.izzu97@gmail.com>
2024-03-26 04:54:45 +0800

    Add Process New (not complete)

---
+++ src/pages/process/ProcessNew.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Helmet } from "react-helmet-async";
+import { Container, Breadcrumb } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+import ProcessNewDetails from "../../components/process/ProcessNewDetails";
+
+const FormNewPage = () => {
+  const navigate = useNavigate();
+
+  const handleUserBreadcrumb = () => {
+    navigate("/processes");
+  };
+
+  return (
+    <React.Fragment>
+      <Helmet title="Process" />
+      <Container fluid className="p-0">
+        <Breadcrumb style={{ fontSize: "1.3rem" }}>
+          <Breadcrumb.Item onClick={handleUserBreadcrumb}>
+            Process
+          </Breadcrumb.Item>
+          <Breadcrumb.Item active>Create</Breadcrumb.Item>
+        </Breadcrumb>
+        <ProcessNewDetails />
+      </Container>
+    </React.Fragment>
+  );
+};
+
+export default FormNewPage;
